bf27be4cce48fc1e850f874dd8ab63328120a786
Jade <13761362605@163.com>
2020-03-28 15:19:56 +0800

    Add files via upload

---
+++ geodata/where.js
@@ -0,0 +1,14 @@
+myData = [
+[31.31480359999999,121.382746, '266 Jufengyuan Rd, Baoshan Qu, Shanghai Shi, China'],
+[50.06688579999999,19.9136192, 'aleja Adama Mickiewicza 30, 30-059 Kraków, Poland'],
+[52.2394019,21.0150792, 'Krakowskie Przedmieście 5, 00-068 Warszawa, Poland'],
+[30.018923,31.499674, 'AUC Avenue، Road، First New Cairo, Cairo Governorate 11835, Egypt'],
+[33.4242399,-111.9280527, 'Tempe, AZ 85281, USA'],
+[33.9094132,-83.4603953, '2500 Daniells Bridge Rd #300, Athens, GA 30606, USA'],
+[28.3588163,75.58802039999999, 'Vidya Vihar, Pilani, Rajasthan 333031, India'],
+[6.8939569,3.7187158, 'Ilishan-Remo, Nigeria'],
+[25.2677203,82.99125819999999, 'Ajagara, Varanasi, Uttar Pradesh 221005, India'],
+[12.9504351,77.5021998, 'Main Rd, Gnana Bharathi, Bengaluru, Karnataka 560056, India'],
+[31.5469132,-97.1210998, '1311 S 5th St, Waco, TX 76706, USA'],
+[39.9619537,116.3662615, '19 Xinjiekou Outer St, Bei Tai Ping Zhuang, Haidian Qu, Beijing Shi, China, 100875']
+];
